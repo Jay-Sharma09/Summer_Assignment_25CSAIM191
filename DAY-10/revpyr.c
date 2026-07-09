be2a83f4199947f026b7d05556712aaeb2db3a91
Jay <jay.sharma2025@glbajajgroup.org>
2026-07-09 22:29:23 +0530

    Added DAY-10 programs

---
+++ DAY-10/revpyr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k, a;
+    printf("Enter number of rows: ");
+    scanf("%d", &a);
+    for(i = a; i >= 1; i--)
+    {
+        for(j = 1; j <= a - i; j++)
+            printf(" ");
+        for(k = 1; k <= (2 * i - 1); k++)
+            printf("*");
+
+        printf("\n");
+    }
+    return 0;
+}
