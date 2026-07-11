312a6b5676343507c29133f0bd73de44d050f8d8
Jay <jay.sharma2025@glbajajgroup.org>
2026-07-11 20:09:15 +0530

    Added DAY-20 programs

---
+++ DAY-20/symm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    int n, i, j, flag = 1;
+    printf("Enter order of matrix: ");
+    scanf("%d", &n);
+    int a[n][n];
+    printf("Enter matrix:\n");
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            if(a[i][j] != a[j][i]){
+                flag = 0;
+            }
+        }
+    }
+    if(flag == 1){
+        printf("Matrix is Symmetric");
+    }
+    else{
+        printf("Matrix is Not Symmetric");
+    }
+    return 0;
+}
